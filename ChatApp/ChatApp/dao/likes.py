9f048900793d6e805fbaf74f09cbad8fccc15d80
drodriguez802 <daniel.rodriguez22@upr.edu>
2018-05-01 20:05:23 -0400

    Phase 2
    +Added GUI functionality
    +Added live DB connection
    +ER Description
    
    -Needs inserts, updates and deletes

--- ChatApp/ChatApp/dao/likes.py
+++ ChatApp/ChatApp/dao/likes.py
@@ -1,37 +1,24 @@
+import psycopg2
 class LikesDAO:
     def __init__(self):
-        P1 = [0, 1, 7, 0]
-        P2 = [1, 0, 4, 3]
-        P3 = [2, 1, 3, 2]
-        P4 = [3, 1, 2, 1]
-        P5 = [4, 1, 4, 1]
-        P6 = [5, 0, 7, 1]
-        P7 = [6, 0, 2, 2]
-        P8 = [7, 0, 1, 2]
+        conn_string = "host='localhost' dbname='chatapp' user='postgres' password='postgres'"
+        self.conn = psycopg2.connect(conn_string)
 
-        self.data = []
-        self.data.append(P1)
-        self.data.append(P2)
-        self.data.append(P3)
-        self.data.append(P4)
-        self.data.append(P5)
-        self.data.append(P6)
-        self.data.append(P7)
-        self.data.append(P8)
+    def getUserReactionsByMessageId(self, id):
+        cursor = self.conn.cursor()
+        cursor.execute("select username from likes natural inner join users where message = %s AND likevalue = 0", (id,))
+        result = [[], []]
+        for row in cursor:
+            result[0].append(row)
+        cursor.execute("select username from likes natural inner join users where message = %s AND likevalue = 1", (id,))
+        for row in cursor:
+            result[1].append(row)
+        return result
 
-    def getAllLikes(self):
-        return self.data
-
-    def getLikesById(self, id):
-        likesByUser = []
-        for r in self.data:
-            if int(id) == r[3]:
-                likesByUser.append(r)
-        return likesByUser
-
-    def getLikesByMessageId(self, id):
-        likesByMessage = []
-        for r in self.data:
-            if int(id) == r[2]:
-                likesByMessage.append(r)
-        return likesByMessage
+    def getAllLikes(self, like):
+        cursor = self.conn.cursor()
+        cursor.execute("select * from likes where likevalue=%s;", like)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
